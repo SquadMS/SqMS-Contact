ebddb4159850ac098940971f1374aecd7f754555
Patrick <privat@skyraptor.eu>
2022-06-12 14:11:10 +0200

    Update ContactServiceProvider.php

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -28,8 +28,5 @@ class ContactServiceProvider extends ServiceProvider
         
         /* Load Migrations */
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-        
-        /* Register Routes */
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
     }
 }
