9fbe1f4b5ffd1ac659b4cc5e79d382153c6cd742
Patrick <privat@skyraptor.eu>
2022-06-12 13:26:07 +0200

    Update Contact.php

--- src/Models/Contact.php
+++ src/Models/Contact.php
@@ -3,6 +3,7 @@
 namespace SquadMS\Contact\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use SquadMS\Foundation\Models\SquadMSUser;
 
 class RuleArticle extends Model
 {
@@ -17,4 +18,39 @@ class RuleArticle extends Model
         'resolution',
         'admin_id'
     ];
+    
+    function user() : BelongsTo
+    {
+        return $this->belongsTo(SquadMSUser::class, 'user_id');
+    }
+    
+    function admin() : BelongsTo
+    {
+        return $this->belongsTo(SquadMSUser::class, 'admin_id');
+    }
+
+    /**
+     * Claims the ContactMessage for an Admin.
+     *
+     * @param User $user
+     * @return void
+     */
+    function claim(User $user) : void
+    {
+        $this->admin()->associate($user);
+        $this->save();
+    }
+
+    /**
+     * Resolves and sets the resolution for the ContactMessage
+     *
+     * @param string $resolution
+     * @return void
+     */
+    function resolve(?string $resolution = '-') : void
+    {
+        $this->update([
+            'resolution' => $resolution,
+        ]);
+    }
 }
