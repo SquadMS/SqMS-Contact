15b5cb2f79883e9e7cbcd80e76578a13f02f8fb1
Patrick <privat@skyraptor.eu>
2022-06-12 13:27:16 +0200

    Update 2022_06_12_130000_create_contact_messages_table.php

--- database/migrations/2022_06_12_130000_create_contact_messages_table.php
+++ database/migrations/2022_06_12_130000_create_contact_messages_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             
             $table->string('email')->nullable();
             $table->unsignedBigInteger('user_id')->nullable();
-            $table->foreign('user_id')->references('id')->on('users')->nullOnDelete();
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
             
             $table->text('subject');
             $table->text('message');
