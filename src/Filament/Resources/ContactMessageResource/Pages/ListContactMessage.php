3d4ee054409425869038af5c5cd5a33018303aa6
Patrick <privat@skyraptor.eu>
2022-06-12 14:42:51 +0200

    Create ListContactMessage.php

---
+++ src/Filament/Resources/ContactMessageResource/Pages/ListContactMessage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SquadMS\Contact\Filament\Resources\ContactMessageResource\Pages;
+
+use SquadMS\Contact\Filament\Resources\ContactMessageResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListEvent extends ListRecords
+{    
+    protected static string $resource = ContactMessageResource::class;
+}
