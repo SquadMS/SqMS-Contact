b9091bf1681bc1e84bdcfb419716c522f351d360
Patrick <privat@skyraptor.eu>
2022-06-12 13:16:11 +0200

    Create sqms-contact.php

---
+++ config/sqms-contact.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+  'permissions' => [
+    'module' => 'sqms-contact',
+    'definitions' => [
+      'admin contact' => 'Grant access to the Contact management',
+    ],
+  ]
+];
