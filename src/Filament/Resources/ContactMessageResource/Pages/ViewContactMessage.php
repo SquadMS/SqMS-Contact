c0a85a8c9893b704ecb2c1b0ad7ecd7c88e5258e
Patrick <privat@skyraptor.eu>
2022-06-12 13:14:18 +0200

    Create ViewContactMessage.php

---
+++ src/Filament/Resources/ContactMessageResource/Pages/ViewContactMessage.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace SquadMS\Contact\Filament\Resources\ContactMessageResource\Pages;
+
+use SquadMS\Contact\Filament\Resources\ContactMessageResource;
+use Filament\Pages\Actions\Action;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewContactMessage extends ViewRecord
+{
+    protected static string $resource = ContactMessageResource::class;
+}
