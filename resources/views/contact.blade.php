d21aeb22f30545d469f891e36b810ba7cd4aebc4
Patrick <privat@skyraptor.eu>
2022-06-15 23:08:27 +0200

    Work on contact template add fields

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,7 +7,36 @@
 @section('page-content')
 <div class="flex flex-wrap">
     <form>
-        <x-sqms-foundation::input type="text" name="subject" />
+
+        <!-- Name -->
+        <div>
+            <x-sqms-foundation::label :value="__('sqms-foundation::contact.fields.name.label')" />
+            <x-sqms-foundation::input type="text" name="name" :placeholder="__('sqms-foundation::contact.fields.name.placeholder')" :value="\Auth::check() ? \Auth::user()->name : old('name')" :readonly="!!\Auth::check()"/>
+            <x-sqms-foundation::input-error for="name" />
+        </div>
+
+        @guest
+        <!-- E-Mail -->
+        <div>
+            <x-sqms-foundation::label :value="__('sqms-foundation::contact.fields.email.label')" />
+            <x-sqms-foundation::input type="text" name="email" :placeholder="__('sqms-foundation::contact.fields.email.placeholder')" :value="old('email')"/>
+            <x-sqms-foundation::input-error for="email" />
+        </div>
+        @endguest
+
+        <!-- Subject -->
+        <div>
+            <x-sqms-foundation::label :value="__('sqms-foundation::contact.fields.subject.label')" />
+            <x-sqms-foundation::input type="text" name="subject" :placeholder="__('sqms-foundation::contact.fields.subject.placeholder')" :value="old('subject')"/>
+            <x-sqms-foundation::input-error for="subject" />
+        </div>
+
+        <!-- Message -->
+        <div>
+            <x-sqms-foundation::label :value="__('sqms-foundation::contact.fields.message.label')" />
+            <textarea name="message" placeholder="{{ __('sqms-foundation::contact.fields.message.placeholder') }}">{{ old('message') }}</textarea>
+            <x-sqms-foundation::input-error for="subject" />
+        </div>
 
         <x-sqms-foundation::button>Send</x-sqms-foundation::button>
     </form>
