94a6dbb4462595b3f3ad8a3a1b7a2821d6c386ff
Patrick <privat@skyraptor.eu>
2022-06-15 23:28:49 +0200

    Translate contact form

--- resources/lang/de/contact.php
+++ resources/lang/de/contact.php
@@ -1,5 +1,27 @@
 <?php
 
 return [
-    'heading' => 'Kontakt'
+    'heading' => 'Kontakt',
+
+    'form' => [
+        'fields' => [
+            'name' => [
+                'label' => 'Name',
+                'placeholder' => 'Dein Spielername',
+            ],
+            'email' => [
+                'label' => 'Name',
+                'placeholder' => 'Deine E-Mail Adresse',
+            ],
+            'subject' => [
+                'label' => 'Name',
+                'placeholder' => 'Dein Anliegen',
+            ],
+            'message' => [
+                'label' => 'Name',
+                'placeholder' => 'Deine Nachricht an uns',
+            ]
+        ],
+        'submit' => 'Senden',
+    ],
 ];
--- resources/lang/en/contact.php
+++ resources/lang/en/contact.php
@@ -1,5 +1,27 @@
 <?php
 
 return [
-    'heading' => 'Contact'
+    'heading' => 'Contact',
+
+    'form' => [
+        'fields' => [
+            'name' => [
+                'label' => 'Name',
+                'placeholder' => 'Your nickname',
+            ],
+            'email' => [
+                'label' => 'E-Mail',
+                'placeholder' => 'Your email address',
+            ],
+            'subject' => [
+                'label' => 'Subject',
+                'placeholder' => 'The subject of your message',
+            ],
+            'message' => [
+                'label' => 'Message',
+                'placeholder' => 'Your message to us',
+            ]
+        ],
+        'submit' => 'Send',
+    ],
 ];
