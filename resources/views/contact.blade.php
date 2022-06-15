b88920dfcc5df3b0b6841113359d9d670904352f
Patrick <privat@skyraptor.eu>
2022-06-15 23:30:24 +0200

    Fix translations namespace in template

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -10,35 +10,35 @@
 
         <!-- Name -->
         <div>
-            <x-sqms-foundation::label :value="__('sqms-foundation::contact.form.fields.name.label')" />
-            <x-sqms-foundation::input type="text" name="name" :placeholder="__('sqms-foundation::contact.form.fields.name.placeholder')" :value="\Auth::check() ? \Auth::user()->name : old('name')" :readonly="!!\Auth::check()"/>
+            <x-sqms-foundation::label :value="__('sqms-contact::contact.form.fields.name.label')" />
+            <x-sqms-foundation::input type="text" name="name" :placeholder="__('sqms-contact::contact.form.fields.name.placeholder')" :value="\Auth::check() ? \Auth::user()->name : old('name')" :readonly="!!\Auth::check()"/>
             <x-sqms-foundation::input-error for="name" />
         </div>
 
         @guest
         <!-- E-Mail -->
         <div>
-            <x-sqms-foundation::label :value="__('sqms-foundation::contact.form.fields.email.label')" />
-            <x-sqms-foundation::input type="text" name="email" :placeholder="__('sqms-foundation::contact.form.fields.email.placeholder')" :value="old('email')"/>
+            <x-sqms-foundation::label :value="__('sqms-contact::contact.form.fields.email.label')" />
+            <x-sqms-foundation::input type="text" name="email" :placeholder="__('sqms-contact::contact.form.fields.email.placeholder')" :value="old('email')"/>
             <x-sqms-foundation::input-error for="email" />
         </div>
         @endguest
 
         <!-- Subject -->
         <div>
-            <x-sqms-foundation::label :value="__('sqms-foundation::contact.form.fields.subject.label')" />
-            <x-sqms-foundation::input type="text" name="subject" :placeholder="__('sqms-foundation::contact.form.fields.subject.placeholder')" :value="old('subject')"/>
+            <x-sqms-foundation::label :value="__('sqms-contact::contact.form.fields.subject.label')" />
+            <x-sqms-foundation::input type="text" name="subject" :placeholder="__('sqms-contact::contact.form.fields.subject.placeholder')" :value="old('subject')"/>
             <x-sqms-foundation::input-error for="subject" />
         </div>
 
         <!-- Message -->
         <div>
-            <x-sqms-foundation::label :value="__('sqms-foundation::contact.form.fields.message.label')" />
-            <textarea name="message" placeholder="{{ __('sqms-foundation::contact.form.fields.message.placeholder') }}">{{ old('message') }}</textarea>
+            <x-sqms-foundation::label :value="__('sqms-contact::contact.form.fields.message.label')" />
+            <textarea name="message" placeholder="{{ __('sqms-contact::contact.form.fields.message.placeholder') }}">{{ old('message') }}</textarea>
             <x-sqms-foundation::input-error for="subject" />
         </div>
 
-        <x-sqms-foundation::button>{{ __('sqms-foundation::contact.form.submit') }}</x-sqms-foundation::button>
+        <x-sqms-foundation::button>{{ __('sqms-contact::contact.form.submit') }}</x-sqms-foundation::button>
     </form>
 </div>
 @endsection
