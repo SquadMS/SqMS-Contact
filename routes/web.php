d5fb105088504798ef62ac47d844053107873b50
Patrick <privat@skyraptor.eu>
2022-08-06 17:54:03 +0200

    Fix routes

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 use SquadMS\Contact\Http\Controllers\ContactController;
 use SquadMS\Foundation\Helpers\SquadMSRouteHelper;
 
-SquadMSRouteHelper::localized(function () {
-    Route::prefix('contact')->name('contact.')->group(function () {
-        Route::get('/', [ContactController::class, 'show'])
-            ->name('show');
 
-        Route::post('/', [ContactController::class, 'send'])
-            ->name('send');
+
+Route::middleware(['web'])->group(function () {
+    SquadMSRouteHelper::localized(function () {
+        Route::prefix('contact')->name('contact.')->group(function () {
+            Route::get('/', [ContactController::class, 'show'])
+                ->name('show');
+    
+            Route::post('/', [ContactController::class, 'send'])
+                ->name('send');
+        });
     });
 });
